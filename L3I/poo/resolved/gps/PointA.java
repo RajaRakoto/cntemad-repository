199d1e6c17fb6bf9374d7983b3929f9302a0eebc
Raja <raja.rakoto7@gmail.com>
2021-10-06 00:06:24 +0300

    [L3i]: add poo 2016 (exo1 - gps XY)

---
+++ L3I/poo/resolved/gps/PointA.java
@@ -0,0 +1,7 @@
+public class PointA extends Point {
+  //methodes
+  public void affiche() {
+    System.out.println("coordonnee X: "+getX());
+    System.out.println("coordonnee Y: "+getY());
+  }
+}
