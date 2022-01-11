ece0446550bf7fb9dd0ba567109c302a4ff2e73e
raja <raja.rakoto7@gmail.com>
2022-01-11 19:38:03 +0300

    [L2I]: reorganize files (techweb)

--- L2I/techweb/hello-L2.js
+++
@@ -1,6 +0,0 @@
-		//a)
-		document.write("Bonjour L2 <br>");
-
-		//b) date de naissance (par la methode prompt)
-		let dateDeNaissance = prompt("Entrer votre date de naissance : ");
-		document.write("Bonjour " + dateDeNaissance);
