17ff2e6ea91c211bbfa3720d7162546e76939d69
raja <raja.rakoto7@gmail.com>
2024-04-03 15:53:36 +0300

    grunt: global update

--- gruntfile.js
+++ gruntfile.js
@@ -1,177 +1,130 @@
 /**
- * @author: Raja
- * @description: task runner for cntemad-repository
- * @requires: grunt | load-grunt-tasks | grunt-contrib-compress | grunt-shell
+ * @description: gruntfile for cntemad repository
+ * @requires: grunt | load-grunt-tasks | grunt-contrib-compress
  */
 module.exports = function (grunt) {
-	require('load-grunt-tasks')(grunt);
+	require("load-grunt-tasks")(grunt);
 
 	// all files destination (example)
-	const backupsDestination = './backups/';
+	const backupsDestination = "./backups/";
 
 	// node-glob syntax
-	const includeAllFiles = ['**/*', '.*/**/*', '**/.*', '**/.*/**/*'];
+	const includeAllFiles = ["**/*", ".*/**/*", "**/.*", "**/.*/**/*"];
 
 	/**
 	 * ~ ALL GRUNT PLUGINS CONFIG ~
 	 */
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('./package.json'),
-
-		/**
-		 * Run shell commands
-		 */
-		shell: {
-			compress_script: {
-				command: 'cd scripts && node compress.js && cd ..',
-			},
-			clear_input: {
-				command: 'cd scripts/src/input && rm -rf *',
-			},
-			clear_output: {
-				command: 'cd scripts/src/output && rm -rf *',
-			},
-		},
-
+		pkg: grunt.file.readJSON("./package.json"),
 		/**
 		 * Compress files and folders (incremental backup)
 		 */
 		compress: {
-			l1i: {
-				options: {
-					archive: backupsDestination + 'L1I.tar.gz',
-				},
-				expand: true,
-				cwd: './L1I/',
-				src: includeAllFiles,
-				dest: 'L1I',
-			},
-			l2i: {
-				options: {
-					archive: backupsDestination + 'L2I.tar.gz',
-				},
-				expand: true,
-				cwd: './L2I/',
-				src: includeAllFiles,
-				dest: 'L2I',
-			},
-			l3i: {
-				options: {
-					archive: backupsDestination + 'L3I.tar.gz',
-				},
-				expand: true,
-				cwd: './L3I/',
-				src: includeAllFiles,
-				dest: 'L3I',
-			},
-			m1i: {
-				options: {
-					archive: backupsDestination + 'M1I.tar.gz',
-				},
-				expand: true,
-				cwd: './M1I/',
-				src: includeAllFiles,
-				dest: 'M1I',
-			},
-			m2i: {
-				options: {
-					archive: backupsDestination + 'M2I.tar.gz',
-				},
-				expand: true,
-				cwd: './M2I/',
-				src: includeAllFiles,
-				dest: 'M2I',
-			},
 			main: {
 				options: {
-					archive: backupsDestination + 'main.tar.gz',
+					archive: backupsDestination + "main.tar.gz",
 				},
-				files: [{ src: ['./*', './.*'] }],
-				filter: 'isFile',
+				files: [{ src: ["./*", "./.*"] }],
+				filter: "isFile",
 			},
 			assets: {
 				options: {
-					archive: backupsDestination + 'assets.tar.gz',
+					archive: backupsDestination + "assets.tar.gz",
 				},
 				expand: true,
-				cwd: './assets/',
+				cwd: "./assets/",
 				src: includeAllFiles,
-				dest: 'assets',
+				dest: "assets",
 			},
-			scripts: {
+			L1I: {
 				options: {
-					archive: backupsDestination + 'scripts.tar.gz',
+					archive: backupsDestination + "L1I.tar.gz",
 				},
 				expand: true,
-				cwd: './scripts/',
+				cwd: "./L1I/",
 				src: includeAllFiles,
-				dest: 'scripts',
+				dest: "L1I",
+			},
+			L2I: {
+				options: {
+					archive: backupsDestination + "L2I.tar.gz",
+				},
+				expand: true,
+				cwd: "./L2I/",
+				src: includeAllFiles,
+				dest: "L2I",
+			},
+      L3I: {
+				options: {
+					archive: backupsDestination + "L3I.tar.gz",
+				},
+				expand: true,
+				cwd: "./L3I/",
+				src: includeAllFiles,
+				dest: "L3I",
+			},
+			M1I: {
+				options: {
+					archive: backupsDestination + "M1I.tar.gz",
+				},
+				expand: true,
+				cwd: "./M1I/",
+				src: includeAllFiles,
+				dest: "M1I",
+			},
+			M2I: {
+				options: {
+					archive: backupsDestination + "M2I.tar.gz",
+				},
+				expand: true,
+				cwd: "./M2I/",
+				src: includeAllFiles,
+				dest: "M2I",
+			},
+			privates: {
+				options: {
+					archive: backupsDestination + "privates.tar.gz",
+				},
+				expand: true,
+				cwd: "./privates/",
+				src: includeAllFiles,
+				dest: "privates",
+			},
+			tmp: {
+				options: {
+					archive: backupsDestination + "tmp.tar.gz",
+				},
+				expand: true,
+				cwd: "./tmp/",
+				src: includeAllFiles,
+				dest: "tmp",
 			},
 		},
 	});
 
 	// all grunt register tasks
-	grunt.registerTask('compress-script', ['shell:compress_script']);
-	grunt.registerTask('clear-all', ['shell:clear_input', 'shell:clear_output']);
-	grunt.registerTask('clear-input', ['shell:clear_input']);
-	grunt.registerTask('clear-output', ['shell:clear_output']);
-	grunt.registerTask('backup-all', [
-		'compress:l1i',
-		'compress:l2i',
-		'compress:l3i',
-		'compress:m1i',
-		'compress:m2i',
-		'compress:main',
-		'compress:assets',
-		'compress:scripts',
+	grunt.registerTask("backup", [
+		"compress:main",
+    "compress:assets",
+    "compress:L1I",
+    "compress:L2I",
+    "compress:L3I",
+    "compress:M1I",
+    "compress:M2I",
+    "compress:privates",
+    "compress:tmp",
 	]);
-	grunt.registerTask('backup-l1i', ['compress:l1i']);
-	grunt.registerTask('backup-l2i', ['compress:l2i']);
-	grunt.registerTask('backup-l3i', ['compress:l3i']);
-	grunt.registerTask('backup-m1i', ['compress:m1i']);
-	grunt.registerTask('backup-m2i', ['compress:m2i']);
-	grunt.registerTask('backup-main', ['compress:main']);
-	grunt.registerTask('backup-assets', ['compress:assets']);
-	grunt.registerTask('backup-scripts', ['compress:scripts']);
 
 	// all tasks lists
-	const tasksNames = [
-		'compress-script',
-		'clear-all',
-		'clear-input',
-		'clear-output',
-		'backup-all',
-		'backup-l1i',
-		'backup-l2i',
-		'backup-l3i',
-		'backup-m1i',
-		'backup-m2i',
-		'backup-main',
-		'backup-assets',
-		'backup-scripts',
-	];
+	const myTasksNames = ["backup"];
 
 	// tasks status (description)
-	const tasksDescription = [
-		'execute compress script',
-		'clear all input and output files from script/src',
-		'clear all input files from script/src',
-		'clear all output files from script/src',
-		'backup all files',
-		'backup L1I files',
-		'backup L2I files',
-		'backup L3I files',
-		'backup M1I files',
-		'backup M2I files',
-		'backup main files',
-		'backup assets files',
-		'backup scripts files',
-	];
+	const myTasksStatus = ["compress: main | assets | L1I | L2I | L3I | M1I | M2I | privates | tmp"];
 
 	// default tasks
-	grunt.registerTask('default', () => {
+	grunt.registerTask("default", () => {
 		console.log(
-			'\nHere are the lists of plugins (tasks) you can run with grunt:'.green,
+			"\nHere are the lists of plugins (tasks) you can run with grunt:".green,
 		);
 
 		/**
@@ -183,31 +136,30 @@ module.exports = function (grunt) {
 		 */
 		function getTaskResume(taskTitle, taskNames, taskStatus, taskTheme) {
 			switch (taskTheme) {
-				case 'cyan':
+				case "cyan":
 					console.log(`\n${taskTitle}`.cyan.inverse.bold);
 					taskNames.forEach((taskNames, index) => {
 						console.log(taskNames.cyan + ` -> ${taskStatus[index]}`);
 					});
 					break;
-				case 'magenta':
+				case "magenta":
 					console.log(`\n${taskTitle}`.magenta.inverse.bold);
 					taskNames.forEach((taskNames, index) => {
 						console.log(taskNames.magenta + ` -> ${taskStatus[index]}`);
 					});
 					break;
-				case 'yellow':
+				case "yellow":
 					console.log(`\n${taskTitle}`.yellow.inverse.bold);
 					taskNames.forEach((taskNames, index) => {
 						console.log(taskNames.yellow + ` -> ${taskStatus[index]}`);
 					});
 					break;
-				case 'blue':
+				case "blue":
 					console.log(`\n${taskTitle}`.blue.inverse.bold);
 					taskNames.forEach((taskNames, index) => {
 						console.log(taskNames.blue + ` -> ${taskStatus[index]}`);
 					});
 					break;
-
 				default:
 					null;
 					break;
@@ -216,10 +168,10 @@ module.exports = function (grunt) {
 
 		// task resume
 		getTaskResume(
-			'=== CNTEMAD REPOSITORY - TASKS ===',
-			tasksNames,
-			tasksDescription,
-			'yellow', // color theme
+			"== CNTEMAD REPOSITORY TASKS ==",
+			myTasksNames,
+			myTasksStatus,
+			"yellow",
 		);
 	});
 };
